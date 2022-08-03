fda27b85eb81a5a42d6714a38213a4a482226ce6
Jesse Cooper <CLJcareer@outlook.com>
2022-08-03 11:22:33 -0500

    Add break statement to stop swapping when no more sorting needs to happen

--- src/main/sort/Insertion.js
+++ src/main/sort/Insertion.js
@@ -1,9 +1,14 @@
 function sort(arr){
 
     for(let i = 1; i < arr.length; i++){
+        // let currentIndex
         for(let j = i; j >= 0; j-- ){
-            if(arr[j - 1] > arr[j])
+            if(arr[j - 1] > arr[j]){
                 swap(j-1,j,arr)
+            } else{
+                break;
+            }
+
         }
     }
 

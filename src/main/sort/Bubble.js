8b5e20dadce9eba490ab9ed9e619da33b1a85c6b
Jesse Cooper <CLJcareer@outlook.com>
2022-08-09 21:36:28 -0500

    Add logic for bubble sort

--- src/main/sort/Bubble.js
+++ src/main/sort/Bubble.js
@@ -1,5 +1,14 @@
 function sort(arr) {
 
+    //Loop from the end to the beginning. This is because as the array become more sorted,
+    //we do not need to look at the right most element anymore.
+    //The inner loop looks at the current element and compares it to the next element.
+    //if the comparison is true, the current element is greater, we swap the elements.
+    //Increment j and check the next element to see if that satisfies the conditional
+    for(let i = arr.length; i >= 0; i--)
+        for(let j = 0; j < i - 1; j++)
+            if(arr[j] > arr[j+1])
+                 swap(j,j+1,arr)
 
     return arr;
 }
